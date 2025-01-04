b4947b46921a01d9c3c66f8f18c792192d18a10d
lucas <lucas.sjc87@hotmail.com>
2025-01-04 20:10:58 -0300

    Inicio do projeto

---
+++ Lanchonete/src/lanchonete/Lanchonete.java
@@ -0,0 +1,13 @@
+
+package lanchonete;
+
+import Telas.telaInicial;
+
+public class Lanchonete {
+
+public static void main(String[] args) {
+       telaInicial tela = new telaInicial();
+       tela.setVisible(true); 
+    }
+    
+}
